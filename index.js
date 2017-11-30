169efa486632a0f3cf6b7a732503082c6d3adfcc
lixiang <freewm@163.com>
2017-11-30 22:10:01 +0800

    finished

---
+++ index.js
@@ -0,0 +1,74 @@
+// 只针对移动端
+;
+(function (win, $) {
+    $(function () {
+        var clientWidth = $('.box')[0].offsetWidth;
+        var start, move, end;
+
+        function setWidth() {
+            $('.line').css('width', clientWidth + 100 + 'px');
+            $('.line .con').css('width', clientWidth + 'px');
+        }
+        $('.box').on('touchstart', 'li', function (e) {
+            var that = null,
+                that = this;
+            var point = e.touches[0];
+            start = {
+                x: point.pageX,
+                y: point.pageY
+            }
+
+        });
+        $('.box').on('touchmove', 'li', function (e) {
+            var that = null,
+                that = this;
+            var point = e.touches[0];
+            move = {
+                x: point.pageX,
+                y: point.pageY
+            };
+            var diffX = move.x - start.x;
+            var diffY = move.y - start.y;
+
+            if (diffX < 0) {
+                console.log("左滑动");
+                $(that).css('transform', 'translateX(' + diffX + 'px)')
+            } else {
+                console.log("右滑动");
+                $(that).css('transform', 'translateX(' + diffX + 'px)')
+            }
+            console.log(diffX, diffY)
+
+        })
+        $('.box').on('touchmove', 'li', function (e) {
+            var that = null;
+            that = this;
+            var point = e.changedTouches[0];
+            end = {
+                x: point.pageX,
+                y: point.pageY
+            };
+            var diffX = end.x - start.x;
+
+            if (diffX < 0) {
+                if (Math.abs(diffX) > 30) {
+                    $(that).css('transform', 'translateX(-100px)')
+                } else {
+                    $(that).css('transform', 'translateX(' + 0 + 'px)')
+                }
+            }else{
+                if (Math.abs(diffX) > 30){
+                    $(that).css('transform', 'translateX(' + 0 + 'px)')
+                }
+            };
+            $(that).find('.btn').click(function(){
+               $(that).remove()
+            })
+
+        })
+
+
+
+        setWidth();
+    })
+})(window, $)
